f5a9ae9e2dd382154429cafe884610aabca0ade8
yaoYiheng <fn1117@qq.com>
2019-02-20 15:30:43 +0800

    任务89完成详细页面UI

--- Client/Assets/Resources/Scripts/UIWindow/UIInfoWindow.cs
+++ Client/Assets/Resources/Scripts/UIWindow/UIInfoWindow.cs
@@ -23,6 +23,18 @@ public class UIInfoWindow : UIWindowRoot
     public Text HPText;
     public Text DamageText;
     public Text DefenceText;
+
+    #region 详情
+    public Transform DetailInfo;
+    public Text DetailHPText;
+    public Text DetailADText;
+    public Text DetailHAPText;
+    public Text DetailAPText;
+    public Text DetailHAPDText;
+    public Text DetailDodgeText;
+    public Text DetailPierceText;
+    public Text DetailCriticalText;
+    #endregion
     #endregion
     private Vector2 StartPos;
     protected override void InitUIwindow()
@@ -64,6 +76,16 @@ public class UIInfoWindow : UIWindowRoot
         UpdateText(HPText, playerData.hp);
         UpdateText(DamageText, playerData.ad + playerData.ap);
         UpdateText(DefenceText, playerData.addef + playerData.apdef);
+
+        //详细信息
+        UpdateText(DetailHPText, playerData.hp);
+        UpdateText(DetailADText, playerData.ad);
+        UpdateText(DetailHAPText, playerData.ap);
+        UpdateText(DetailAPText, playerData.addef);
+        UpdateText(DetailHAPDText, playerData.apdef);
+        UpdateText(DetailDodgeText, playerData.dodge + "%");
+        UpdateText(DetailPierceText, playerData.pierce + "%");
+        UpdateText(DetailCriticalText, playerData.critical + "%");
     }
 
     public void ShowInfoWindow()
@@ -80,5 +102,13 @@ public class UIInfoWindow : UIWindowRoot
         AudioService.Instance.PlayEffect(Consts.A_MenuButtonClick);
         MainCitySystem.Instance.HideInfowWindow();
     }
+    public void OnDetailButtonClick()
+    {
+        SetActive(DetailInfo);
+    }
+    public void OnDetailCloseButtonClick()
+    {
 
+        SetActive(DetailInfo, false);
+    }
 }
