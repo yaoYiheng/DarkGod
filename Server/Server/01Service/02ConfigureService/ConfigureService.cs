ba409c386788f81a3640c59217ab4d9eb8b2f38c
yaoYiheng <fn1117@qq.com>
2019-02-24 14:42:38 +0800

    任务104: 增加服务器端数据配置服务器
    类似于客户端版的资源服务器, 但是服务器中不需要加载入prefab之类的资源, 只需要从硬盘中读取配置文件即可.
    照客户端版中的加载配置资源一样, 值保留所需要的字段以及数据即可

---
+++ Server/Server/01Service/02ConfigureService/ConfigureService.cs
@@ -0,0 +1,77 @@
+﻿// EmptyClass.cs
+// yiheng
+// 
+// 20192241421
+using System;
+using System.Collections.Generic;
+using System.Xml;
+
+public class ConfigureService : Singelton<ConfigureService>
+{
+
+
+    public override void Init()
+    {
+        Common.Log("服务器端配置初始化");
+        InitGuideConfigure();
+    }
+
+    #region 获取自动任务配置
+    public AutoGuideConfigures GetGuideConfigures(int guideID)
+    {
+        AutoGuideConfigures guideConfigures = null;
+        if (m_GuideDict.TryGetValue(guideID, out guideConfigures))
+        {
+            return guideConfigures;
+        }
+        return null;
+    }
+
+    private Dictionary<int, AutoGuideConfigures> m_GuideDict = new Dictionary<int, AutoGuideConfigures>();
+    private void InitGuideConfigure()
+    {
+
+        XmlDocument xml = new XmlDocument();
+        xml.Load(@"/Users/yiheng/Documents/Unity相关/DarkGod/Client/Assets/Resources/ResCfgs/guide.xml");
+        XmlNodeList list = xml.SelectSingleNode("root").ChildNodes;
+        for (int i = 0; i < list.Count; i++)
+        {
+            var element = list[i] as XmlElement;
+            if (element.GetAttributeNode("ID") == null) continue;
+
+            var autoGuide = new AutoGuideConfigures()
+            {
+                ID = int.Parse(element.GetAttributeNode("ID").InnerText)
+            };
+            foreach (XmlElement item in list[i].ChildNodes)
+            {
+                switch (item.Name)
+                {
+                    case "coin":
+                        autoGuide.coin = int.Parse(item.InnerText);
+                        break;
+                    case "exp":
+                        autoGuide.exp = int.Parse(item.InnerText);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            m_GuideDict.Add(autoGuide.ID, autoGuide);
+        }
+
+    }
+    #endregion
+}
+public class AutoGuideConfigures : BaseData<AutoGuideConfigures>
+{
+
+    public int coin;
+    public int exp;
+}
+
+public class BaseData<T>
+{
+    public int ID;
+}
